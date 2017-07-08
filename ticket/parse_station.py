392afa662b0eaac1606ee39589387e97666537fe
severalfly <zhangsmile90@gmail.com>
2017-07-08 22:17:30 +0800

    12306 station

--- ticket/parse_station.py
+++ ticket/parse_station.py
@@ -2,10 +2,6 @@ import re
 import requests
 from pprint import pprint 
 
-'''
-自动下载12306站点信息
-'''
-
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
 response = requests.get(url, verify=False)
 stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
