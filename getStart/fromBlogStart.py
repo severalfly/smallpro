72baef466ce5b738f0f5e9626b9d582bfd79090d
leon <leon@localhost.localdomain>
2017-07-23 07:51:51 +0800

    a start sample from blog

---
+++ getStart/fromBlogStart.py
@@ -0,0 +1,11 @@
+#_*_coding:utf-8_*_
+import os.path
+import re
+import sys
+import numpy as np
+import urllib.request as req
+import tensorflow as tf
+
+print('hello world')
+FLAGS  = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string
