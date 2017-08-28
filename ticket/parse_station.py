5424672a1a34837449abc68cbda5ab366e5647a5
severalfly <zhangsmile90@gmail.com>
2017-08-29 07:36:12 +0800

    start django

--- ticket/parse_station.py
+++ ticket/parse_station.py
@@ -2,7 +2,9 @@ import re
 import requests
 from pprint import pprint 
 
-url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
+# url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
+url = 'http://dynamic.12306.cn/mapping/kfxt/zwdcx/LCZWD/from2.jsp?cz=%BA%BA%BF%DA&cc=T180&cxlx=0&rq=2017-08-28&czEn=-E6-B1-89-E5-8F-A3&tp=1503931254778'
 response = requests.get(url, verify=False)
+response.
 stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
 pprint(dict(stations), indent=4)
