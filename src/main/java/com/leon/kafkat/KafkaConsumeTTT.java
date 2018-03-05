d56860c229b4edb9e7b5f1c97d62d11b619e42ea
severalfly <zhangsmile90@gmail.com>
2018-03-05 14:53:48 +0800

    kafka can run produce

---
+++ src/main/java/com/leon/kafkat/KafkaConsumeTTT.java
@@ -0,0 +1,67 @@
+package com.leon.kafkat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class KafkaConsumeTTT
+{
+	private final static String TOPIC = "test-topic";
+
+	private static Properties properties;
+
+	static
+	{
+		properties = new Properties();
+
+		properties.put("zookeeper.connect", "localhost:2181");
+		properties.put("group.id", "lijiegroup");
+		properties.put("zookeeper.session.timeout.ms", "4000");
+		properties.put("zookeeper.sync.time.ms", "200");
+		properties.put("auto.commit.interval.ms", "1000");
+		properties.put("auto.offset.reset", "smallest");
+		properties.put("serializer.class", "org.apache.kafka.common.serialization.StringSerializer");
+	}
+
+	/**
+	 * 获取消息
+	 * 
+	 * @throws Exception
+	 */
+	public void getMsg() throws Exception
+	{
+		//		ConsumerConfig config = new ConsumerConfig(properties);
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer(properties);
+
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+
+		topicCountMap.put(TOPIC, new Integer(1));
+
+		//		StringDecoder keyDecoder = new StringDecoder(new VerifiableProperties());
+		//
+		//		StringDecoder valueDecoder = new StringDecoder(new VerifiableProperties());
+
+		Map<String, ConsumerRecords<String, String>> records = consumer.poll(10000);
+		System.out.println(JSONObject.toJSONString(records));
+		consumer.close();
+		//		Map<String, List<KafkaStream<String, String>>> consumerMap = consumer.createMessageStreams(topicCountMap, keyDecoder, valueDecoder);
+		//
+		//		KafkaStream<String, String> stream = consumerMap.get(TOPIC).get(0);
+		//
+		//		ConsumerIterator<String, String> it = stream.iterator();
+		//
+		//		while (it.hasNext())
+		//		{
+		//			String json = it.next().message();
+		//			User user = (User) JsonUtils.JsonToObj(json, User.class);
+		//			System.out.println(user);
+		//		}
+	}
+
+}
---
+++ src/main/java/com/leon/kafkat/KafkaProducerTTT.java
@@ -0,0 +1,68 @@
+package com.leon.kafkat;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class KafkaProducerTTT
+{
+	private final Producer<String, String> producer;
+	public final static String TOPIC = "my-replicated-topic";
+
+	private KafkaProducerTTT()
+	{
+		Properties props = new Properties();
+		//此处配置的是kafka的端口
+		//		props.put("metadata.broker.list", "localhost:9092");
+		props.put("broker.list", "192.168.102.45:9092,192.168.102.45:9093,192.168.102.45:9094");
+
+		//配置value的序列化类
+		//		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		//配置key的序列化类
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("bootstrap.servers", "192.168.102.45:9092,192.168.102.45:9093,192.168.102.45:9094");
+		//		props.put("serializer.class", "kafka.serializer.StringEncoder");
+
+		//request.required.acks
+		//0, which means that the producer never waits for an acknowledgement from the broker (the same behavior as 0.7). This option provides the lowest latency but the weakest durability guarantees (some data will be lost when a server fails).
+		//1, which means that the producer gets an acknowledgement after the leader replica has received the data. This option provides better durability as the client waits until the server acknowledges the request as successful (only messages that were written to the now-dead leader but not yet replicated will be lost).
+		//-1, which means that the producer gets an acknowledgement after all in-sync replicas have received the data. This option provides the best durability, we guarantee that no messages will be lost as long as at least one in sync replica remains.
+		props.put("request.required.acks", "-1");
+
+		producer = new KafkaProducer(props);
+		//		producer.
+		//		ProducerConfig i = new ProducerConfig
+		//		producer = new Producer<String, String>();
+	}
+
+	void produce()
+	{
+		int messageNo = 1;
+		final int COUNT = 10;
+
+		while (messageNo < COUNT)
+		{
+			String key = String.valueOf(messageNo);
+			String data = new Date() + " hello kafka message " + key;
+			ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, data);
+			Future<RecordMetadata> i = producer.send(record);
+			System.out.println(JSONObject.toJSONString(i));
+			//			producer.send(new KeyedMessage<String, String>(TOPIC, key, data));
+			System.out.println(data);
+			messageNo++;
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		new KafkaProducerTTT().produce();
+	}
+}
