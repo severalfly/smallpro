4622e8e7ba823188a9954605383d95dcd7097af1
张云飞 <zhangsmile90@gmail.com>
2018-04-12 23:11:09 +0800

    start to fetch

--- src/main/java/com/leon/passwd/schedule/GrabLeon.java
+++ src/main/java/com/leon/passwd/schedule/GrabLeon.java
@@ -19,23 +19,33 @@ public class GrabLeon extends Thread {
 	@Override
 	public void run() {
 		try {
+			// 当前失败次数，超过5次时，就可以退出了
+			int errCnt = 0;
+			int failCnt = 0;
 			for (int i = 0; i < 100; i++) {
 
 				JSONObject json = queryFrom12306();
 				if (json != null) {
 					if (json.getBooleanValue("status") && "1".equals(json.getJSONObject("data").getString("flag"))) {
 						// 暂时认为的成功状态
-						logger.info("获取到正确结果一次： " + new Date().toString());
+						logger.info(i + "获取到正确结果一次： " + new Date().toString());
 					} else {
 						// 返回成功，但是结果失败
+						failCnt++;
 						logger.info("获取到失败结果一次： " + new Date().toString());
 					}
 					// System.out.println(json.toJSONString());
 				} else {
 					System.out.println("error");
+					errCnt++;
 				}
-				Thread.sleep(5 * 1000);
+				if (errCnt > 4 || failCnt > 4) {
+					logger.info(new Date() + " 当前已失败 errCnt: {}  failCnt: {} 退出抓取任务, 总执行: " + i, errCnt, failCnt);
+					break;
+				}
+				Thread.sleep(1 * 100);
 			}
+			logger.info(new Date() + " 当前已完成 errCnt: {}  failCnt: {} 退出抓取任务", errCnt, failCnt);
 		} catch (Exception e) {
 			logger.error("哇，有异常", e);
 
@@ -51,7 +61,7 @@ public class GrabLeon extends Thread {
 			String departDate = "2018-04-30";
 			// String url =
 			// "https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT";
-			String url = "https://kyfw.12306.cn/otn/leftTicket/queryO?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT";
+			String url = "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date=%s&leftTicketDTO.from_station=%s&leftTicketDTO.to_station=%s&purpose_codes=ADULT";
 			url = String.format(url, departDate, departCode, arriveCode);
 			HttpGet get = new HttpGet(url);
 
@@ -60,7 +70,7 @@ public class GrabLeon extends Thread {
 			get.addHeader("Host", "kyfw.12306.cn");
 			get.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:41.0) Gecko/20100101 Firefox/51.0");
 			get.addHeader("Cookie",
-					"JSESSIONID=A5B5F4A74250FF94E6C60F77713B15E8; route=9036359bb8a8a461c164a04f8f50b252; BIGipServerotn=132383242.64545.0000; RAIL_EXPIRATION=1523841767777; RAIL_DEVICEID=csxLbdO1UzYEXGNRwWpoIN3pb6LjGLpo7PlJtLzEW_ef94Mz3LqKg-rdNG4oG1NKww2nJ7yEyInR5C0VR9n0MyTDBjEiaT7LjikhKhE3KFF5zRNP4eUd7722EK0eZGeaH1rPKu_Yr71lPoGqRsPyRPHFoWhJkStx; _jc_save_fromStation=%u6B66%u6C49%2CWHN; _jc_save_toStation=%u5317%u4EAC%2CBJP; _jc_save_fromDate=2018-04-20; _jc_save_toDate=2018-04-12; _jc_save_wfdc_flag=dc");
+					"JSESSIONID=18B19BCA6F2988B5A5FF9E31317563CC; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=350487050.38945.0000; BIGipServerpool_passport=200081930.50215.0000; _jc_save_detail=true; current_captcha_type=Z; _jc_save_wfdc_flag=dc; BIGipServeropn=1592787466.24610.0000; _jc_save_fromStation=%u8302%u540D%2CMDQ; _jc_save_toStation=%u8087%u5E86%2CZVQ; RAIL_EXPIRATION=1523597209057; RAIL_DEVICEID=OPlWqemc1vvR9uPU0NpqhsNk4Bg8A64SCm3SXMg8jpWL79YiAiQ__KwxDlXMROPl3ybwPFan3GybXGk4eO19Bcj7dogBfLe5j3IfQInqiC6tDpZTFuEtUhOg0VjGqVjqggj14rKAEyhPAhvGwftUAj-tPr-4N649; _jc_save_fromDate=2018-04-12; _jc_save_toDate=2018-04-12");
 			// get.setConfig(config);
 
 			// set12306Cookie(server.getRight(), context);
