7e22ee2370b6f2ebdb536d086f0aea14b97268a8
张云飞 <zhangsmile90@gmail.com>
2018-04-13 08:24:55 +0800

    test

--- src/main/java/com/leon/passwd/T.java
+++ src/main/java/com/leon/passwd/T.java
@@ -1,5 +1,13 @@
 package com.leon.passwd;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 public class T {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String str = "%u6B66%u6C49%2C";
+		System.out.println(URLDecoder.decode(str, "utf-8"));
+	}
 
 }
--- src/main/java/com/leon/passwd/schedule/GrabLeon.java
+++ src/main/java/com/leon/passwd/schedule/GrabLeon.java
@@ -18,44 +18,49 @@ public class GrabLeon extends Thread {
 
 	@Override
 	public void run() {
+		logger.info("grab leon start...");
 		try {
 			// 当前失败次数，超过5次时，就可以退出了
 			int errCnt = 0;
 			int failCnt = 0;
-			for (int i = 0; i < 100; i++) {
+			int count = 500;
+			int limit = count * 2 / 100; // 配置失败率后退出，
+			for (int i = 0; i < count; i++) {
 
-				JSONObject json = queryFrom12306();
+				JSONObject json = queryFrom12306(false);
 				if (json != null) {
 					if (json.getBooleanValue("status") && "1".equals(json.getJSONObject("data").getString("flag"))) {
 						// 暂时认为的成功状态
-						logger.info(i + "获取到正确结果一次： " + new Date().toString());
+						logger.info("第 " + i + " 次获取到正确结果一次： " + new Date().toString() + "  当前已失败 errCnt: {}  failCnt: {}", errCnt, failCnt);
 					} else {
 						// 返回成功，但是结果失败
 						failCnt++;
-						logger.info("获取到失败结果一次： " + new Date().toString());
+						logger.info("第 " + i + " 获取到失败结果一次： " + new Date().toString());
 					}
 					// System.out.println(json.toJSONString());
 				} else {
 					System.out.println("error");
 					errCnt++;
 				}
-				if (errCnt > 4 || failCnt > 4) {
+				if (errCnt > limit || failCnt > limit) {
 					logger.info(new Date() + " 当前已失败 errCnt: {}  failCnt: {} 退出抓取任务, 总执行: " + i, errCnt, failCnt);
 					break;
 				}
-				Thread.sleep(1 * 100);
+				Thread.sleep(8 * 10);
+				if (i % 10 == 0) {
+					Thread.sleep(22 * 100);
+				}
 			}
-			logger.info(new Date() + " 当前已完成 errCnt: {}  failCnt: {} 退出抓取任务", errCnt, failCnt);
+			logger.info(new Date() + " 当前 " + count + " 已完成 errCnt: {}  failCnt: {} 退出抓取任务", errCnt, failCnt);
 		} catch (Exception e) {
 			logger.error("哇，有异常", e);
 
 		}
 	}
 
-	private JSONObject queryFrom12306() {
+	private JSONObject queryFrom12306(boolean logFlag) {
 
 		try {
-			logger.info("grab leon start...");
 			String departCode = "WHN";
 			String arriveCode = "BJP";
 			String departDate = "2018-04-30";
@@ -70,7 +75,7 @@ public class GrabLeon extends Thread {
 			get.addHeader("Host", "kyfw.12306.cn");
 			get.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:41.0) Gecko/20100101 Firefox/51.0");
 			get.addHeader("Cookie",
-					"JSESSIONID=18B19BCA6F2988B5A5FF9E31317563CC; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=350487050.38945.0000; BIGipServerpool_passport=200081930.50215.0000; _jc_save_detail=true; current_captcha_type=Z; _jc_save_wfdc_flag=dc; BIGipServeropn=1592787466.24610.0000; _jc_save_fromStation=%u8302%u540D%2CMDQ; _jc_save_toStation=%u8087%u5E86%2CZVQ; RAIL_EXPIRATION=1523597209057; RAIL_DEVICEID=OPlWqemc1vvR9uPU0NpqhsNk4Bg8A64SCm3SXMg8jpWL79YiAiQ__KwxDlXMROPl3ybwPFan3GybXGk4eO19Bcj7dogBfLe5j3IfQInqiC6tDpZTFuEtUhOg0VjGqVjqggj14rKAEyhPAhvGwftUAj-tPr-4N649; _jc_save_fromDate=2018-04-12; _jc_save_toDate=2018-04-12");
+					"JSESSIONID=19463F5BF04481182AF9E14C1319AB6D; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerotn=350487050.38945.0000; BIGipServerpool_passport=200081930.50215.0000; _jc_save_detail=true; current_captcha_type=Z; _jc_save_wfdc_flag=dc; BIGipServeropn=1592787466.24610.0000; RAIL_EXPIRATION=1523597209057; RAIL_DEVICEID=OPlWqemc1vvR9uPU0NpqhsNk4Bg8A64SCm3SXMg8jpWL79YiAiQ__KwxDlXMROPl3ybwPFan3GybXGk4eO19Bcj7dogBfLe5j3IfQInqiC6tDpZTFuEtUhOg0VjGqVjqggj14rKAEyhPAhvGwftUAj-tPr-4N649; _jc_save_fromStation=%u6B66%u6C49%2CWHN; _jc_save_toStation=%u5317%u4EAC%2CBJP; _jc_save_fromDate=2018-04-13; _jc_save_toDate=2018-04-13");
 			// get.setConfig(config);
 
 			// set12306Cookie(server.getRight(), context);
@@ -78,12 +83,16 @@ public class GrabLeon extends Thread {
 			HttpResponse response = httpClient.execute(get);
 			HttpEntity entity = response.getEntity();
 			// System.out.println(EntityUtils.toString(entity));
-			logger.info("获取到结果全部结果 {}", JSONObject.toJSONString(response));
+			if (logFlag)
+				logger.info("获取到结果全部结果 {}", JSONObject.toJSONString(response));
 			String sEntity = EntityUtils.toString(entity);
-			logger.info("获取到结果entity结果 {}", sEntity);
+			if (logFlag || !sEntity.startsWith("{"))
+				logger.info("获取到结果entity结果 {}", sEntity);
 			// System.out.println(JSONObject.toJSONString(response));
-			JSONObject json = JSONObject.parseObject(sEntity);
-			return json;
+			if (sEntity.startsWith("{")) {
+				JSONObject json = JSONObject.parseObject(sEntity);
+				return json;
+			}
 
 		} catch (Exception e) {
 			logger.error("", e);
