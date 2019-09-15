f55fa0d61319aae6021d0aab0a6924947668df47
张云飞 <zhangsmile90@gmail.com>
2019-09-15 20:34:19 +0800

    补充log4j

--- src/main/java/com/leon/passwd/action/AccountAction.java
+++ src/main/java/com/leon/passwd/action/AccountAction.java
@@ -1,20 +1,29 @@
 package com.leon.passwd.action;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.struts2.convention.annotation.Action;
+
 import java.util.Date;
 
-import org.apache.struts2.convention.annotation.Action;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+@Slf4j
 public class AccountAction extends AbstractAction
 {
 	private static final long serialVersionUID = 5601982197489875967L;
-	private static Logger logger = LoggerFactory.getLogger(AccountAction.class);
+	//	private static Logger logger = LoggerFactory.getLogger(AccountAction.class);
 
 	@Action(value = "testv2")
 	public void testv2()
 	{
-		logger.info("testv2 " + new Date());
+		log.info("testv2 " + new Date());
 		System.out.println("testv2 " + new Date());
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			log.info("Info log [" + i + "].");
+			Thread.sleep(500);
+		}
 	}
 }
