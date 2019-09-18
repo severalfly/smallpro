970008ccb489d970c8f2726f64ab2e8489af7ca4
张云飞 <zhangsmile90@gmail.com>
2019-09-18 21:18:49 +0800

    支持日志

--- src/main/java/com/leon/passwd/action/AccountAction.java
+++ src/main/java/com/leon/passwd/action/AccountAction.java
@@ -1,28 +1,32 @@
 package com.leon.passwd.action;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.struts2.convention.annotation.Action;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Slf4j
+@RestController
 public class AccountAction extends AbstractAction
 {
 	private static final long serialVersionUID = 5601982197489875967L;
 	//	private static Logger logger = LoggerFactory.getLogger(AccountAction.class);
 
-	@Action(value = "testv2")
-	public void testv2()
+	@RequestMapping(value = "testv2")
+	public String testv2()
 	{
 		log.info("testv2 " + new Date());
 		System.out.println("testv2 " + new Date());
+		return "done";
 	}
 
 	public static void main(String[] args) throws Exception
 	{
 		for (int i = 0; i < 10; i++)
 		{
-			log.info("Info log [" + i + "].");
+			log.info("Info log [" + i + "]." + new Timestamp(System.currentTimeMillis()).toString());
 			Thread.sleep(500);
 		}
 	}
