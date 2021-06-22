a274772c472580c04f54d4913704c8c9ec434b7e
张云飞 <zhangyf@133.cn>
2021-06-22 08:13:54 +0800

    初始化

---
+++ src/main/java/com/leon/tom/ex/StaticResourceProcessor.java
@@ -0,0 +1,18 @@
+package com.leon.tom.ex;
+
+import ex03.pyrmont.connector.http.HttpRequest;
+import ex03.pyrmont.connector.http.HttpResponse;
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+  public void process(HttpRequest request, HttpResponse response) {
+    try {
+      response.sendStaticResource();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
